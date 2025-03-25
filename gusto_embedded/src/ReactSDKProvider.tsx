dfd17c7131b217d9bb604e71c0f5d398836a268b
Aaron Rosen <aaron.rosen@gusto.com>
2025-03-25 13:34:09 -0600

    pass headers into the ReactSDKProvider

--- gusto_embedded/src/ReactSDKProvider.tsx
+++ gusto_embedded/src/ReactSDKProvider.tsx
@@ -1,24 +1,51 @@
+import { HTTPClient } from "./lib/http.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GustoEmbeddedProvider } from "./react-query/index.js";
 import { GustoEmbeddedCore } from "./core.js";
 
 //Reexporting useQueryClient to be available in consumer application without direct react-query dependency
-export { useQueryClient } from "@tanstack/react-query"
+export { useQueryClient } from "@tanstack/react-query";
 
-export function ReactSDKProvider({ url, children }: { url: string, children: React.ReactNode }) {
-    const queryClient = new QueryClient();
-    const gustoClient = new GustoEmbeddedCore({
-        serverURL: url,
-    },)
-    queryClient.setQueryDefaults(['@gusto/embedded-api'], { retry: false })
-    queryClient.setMutationDefaults(['@gusto/embedded-api'], { retry: false })
+export function ReactSDKProvider({
+  url,
+  headers,
+  children,
+}: {
+  url: string;
+  headers?: Record<string, string | number>;
+  children: React.ReactNode;
+}) {
+  const httpClientWithHeaders = new HTTPClient({
+    fetcher: async (request) => {
+      if (request instanceof Request) {
+        const requestWithHeaders = {
+          ...request,
+          headers: {
+            ...request.headers,
+            ...headers,
+          },
+        };
+        return fetch(requestWithHeaders);
+      }
+      return fetch(request);
+    },
+  });
 
+  const queryClient = new QueryClient();
+  const gustoClient = new GustoEmbeddedCore({
+    serverURL: url,
+    httpClient: httpClientWithHeaders,
+  });
+  queryClient.setQueryDefaults(["@gusto/embedded-api"], { retry: false });
+  queryClient.setMutationDefaults(["@gusto/embedded-api"], { retry: false });
 
-    return (<QueryClientProvider client={queryClient} >
-        <GustoEmbeddedProvider client={gustoClient}>
-            {children}
-        </GustoEmbeddedProvider>
-    </QueryClientProvider>)
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GustoEmbeddedProvider client={gustoClient}>
+        {children}
+      </GustoEmbeddedProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default { ReactSDKProvider }
+export default { ReactSDKProvider };
